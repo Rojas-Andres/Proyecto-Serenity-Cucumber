5635c3d17ea05e257b948ce0e32f8a2212cad90e
RojasxAndres <afrojaslondono@gmail.com>
2021-02-19 12:29:32 -0500

    Implementación datadriven y clase Model

--- src/test/java/proyecto2/stepdefinitions/ChoucairAcademyStefDefinitions.java
+++ src/test/java/proyecto2/stepdefinitions/ChoucairAcademyStefDefinitions.java
@@ -9,10 +9,14 @@ import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 //import org.mockito.stubbing.Answer;
+import proyecto2.model.AcademyChoucairData;
 import proyecto2.task.Login;
 import proyecto2.task.OpenUp;
 import proyecto2.task.Search;
 import proyecto2.questions.Answer;
+
+import java.util.List;
+
 public class ChoucairAcademyStefDefinitions {
     //Preparamos el scenario para el actor
     @Before
@@ -21,24 +25,24 @@ public class ChoucairAcademyStefDefinitions {
     }
 
     @Given("^Than branon wants to learn automation at the academy choucair$")
-    public void thanBranonWantsToLearnAutomationAtTheAcademyChoucair() {
+    public void thanBranonWantsToLearnAutomationAtTheAcademyChoucair(List<AcademyChoucairData> academyChoucairData) throws Exception {
         //OpenUp esta en task y creamos precondiciones
-        OnStage.theActorCalled("branon").wasAbleTo(OpenUp.thePage(),(Login.onThePage()));
+        OnStage.theActorCalled("branon").wasAbleTo(OpenUp.thePage(),(Login.onThePage(academyChoucairData.get(0).getStrUser(),academyChoucairData.get(0).getStrPassword())));
 
     }
 
 
-    @When("^he search for the course (.*) on the choucair academy plataform$")
-    public void heSearchForTheCourseRecursosAutomatizacionBancolombiaOnTheChoucairAcademyPlataform(String course) {
+    @When("^he search for the course on the choucair academy plataform$")
+    public void heSearchForTheCourseRecursosAutomatizacionBancolombiaOnTheChoucairAcademyPlataform(List<AcademyChoucairData> academyChoucairData) throws Exception {
         // Write code here that turns the phrase above into concrete actions
-        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(course));
+        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(academyChoucairData.get(0).getStrCourse()));
 
     }
 
-    @Then("^he finds the course called resources (.*)$")
-    public void heFindsTheCourseCalledResourcesRecursosAutomatizacionBancolombia(String question) {
+    @Then("^he finds the course called resources$")
+    public void heFindsTheCourseCalledResourcesRecursosAutomatizacionBancolombia(List<AcademyChoucairData> academyChoucairData) throws Exception {
 
         // Write code here that turns the phrase above into concrete actions
-        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(academyChoucairData.get(0).getStrCourse())));
     }
 }
--- src/test/resources/features/academyChoucair.feature
+++ src/test/resources/features/academyChoucair.feature
@@ -2,7 +2,17 @@
   Feature: Academy Choucair
     As a user ,I want to learn how to autamate in screamplay at the choucair Academy whit automatization cou
   @scenario1
-  Scenario: Search for automatization course
+  Scenario Outline: Search for automatization course
     Given Than branon wants to learn automation at the academy choucair
-    When he search for the course Workshop de Automatización on the choucair academy plataform
-    Then he finds the course called resources Workshop de Automatización
+    | strUser    | strPassword       |
+    | <strUser>  | <strPassword>     |
+    When he search for the course on the choucair academy plataform
+    | strCourse   |
+    | <strCourse> |
+    Then he finds the course called resources
+    | strCourse   |
+    | <strCourse> |
+
+    Examples:
+    | strUser    | strPassword   | strCourse                  |
+    | 1006206400 | Choucair2021* | Workshop de Automatización |
